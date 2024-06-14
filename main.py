9070a67bd9878e13bbce972d5b6413c31598dab3
Stormyisland <jacqueslavoie2015@gmail.com>
2024-06-13 18:41:59 -0700

    3 commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-1
+from torch import nn
+from torch.optim import adam 
+from torch .utils.data import  DataLoader
+from torchvision import datasets 
+from torchvision.transformer import TOTensor
+
+train = datasets.MNIST(root = "data, dowload=True, train=True, transform=ToTensor)
+dataset = DataLoader(train, 32)
+                       
+                       
